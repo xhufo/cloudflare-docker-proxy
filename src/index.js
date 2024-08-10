47912b84a24b8be27a8122f647658dfc39dbedad
UFO <xhufo@users.noreply.github.com>
2024-08-11 01:22:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ufoqifei.us.kg": dockerHub,
+  "quay.ufoqifei.us.kg": "https://quay.io",
+  "gcr.ufoqifei.us.kg": "https://gcr.io",
+  "k8s-gcr.ufoqifei.us.kg": "https://k8s.gcr.io",
+  "k8s.ufoqifei.us.kg": "https://registry.k8s.io",
+  "ghcr.ufoqifei.us.kg": "https://ghcr.io",
+  "cloudsmith.ufoqifei.us.kg": "https://docker.cloudsmith.io",
+  "ecr.ufoqifei.us.kg": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
